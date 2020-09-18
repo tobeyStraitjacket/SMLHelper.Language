66347c9b60b0d4f3077b6bcf1304b27aa16a46ab
Tobey Blaber <tobey@straitjacket.software>
2020-09-18 23:59:01 +0100

    Initial commit

---
+++ Language/Language.cs
@@ -0,0 +1,16 @@
+﻿using SMLHelper.V2.Handlers;
+using System.Collections.Generic;
+
+namespace SMLHelper
+{
+    internal static class Language
+    {
+        public static string Get(string id) => global::Language.main.Get(id);
+        public static void Set(string id, string value) => LanguageHandler.Main.SetLanguageLine(id, value);
+        public static string Set(Dictionary<string, string> dictionary)
+        {
+            foreach (var entry in dictionary)
+                Set(entry.Key, entry.Value);
+        }
+    }
+}
